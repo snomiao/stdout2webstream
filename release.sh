529b09bb2fd82746992d9dc78c8f08eb496e9341
snomiao <snomiao@gmail.com>
2024-02-11 09:25:16 +0000

    chore(release): script
    script

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
+npm version patch
 docker compose build
+npm publish
 # docker rm -f stdout2webstream
 # docker run -it -v ${PWD}/index.ts:/app/index.ts --rm --name stdout2webstream --network snocode_default ghcr.io/snomiao/stdout2webstream bun --hot index.ts
 docker compose push
